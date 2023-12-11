7f5fa1776b581d5eb161bfa2db3e1f582bf96d9f
Fadil-Tao <hadadfadilah2354@gmail.com>
2023-12-11 20:26:24 +0800

    Updating the main jsx route and index css

---
+++ src/main.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import CollectionPage from './Pages/CollectionPage';
+import CartPage from './Pages/CartPage';
+import RootLayout from './Components/Layouts/RootLayout';
+import ProductDetail from './Pages/ProductDetailPage';
+import { SnackbarProvider } from 'notistack';
+import Footer from './Components/Layouts/FooterLayout';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout />,
+        children: [
+            {
+                path: '/',
+                element: <ProductDetail />,
+            },
+            {
+                path: '/home',
+                element: <HomePage />,
+            },
+            {
+                path: '/collection',
+                element: <CollectionPage />,
+            },
+            {
+                path: '/mycart',
+                element: <CartPage />,
+            },
+            {
+                path: '/collection/:id',
+                element: (
+                    <SnackbarProvider>
+                        <ProductDetail />
+                    </SnackbarProvider>
+                ),
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
+);
